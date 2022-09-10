63f004026c6d5a66293fbc7540d8cec0eaad1cff
Serge Fedosov <107842308+serge-fedosov@users.noreply.github.com>
2022-09-10 13:20:19 +0400

    Stage 2/5 done

--- ASCII Mirror/task/src/asciimirror/Main.java
+++ ASCII Mirror/task/src/asciimirror/Main.java
@@ -1,15 +1,23 @@
 package asciimirror;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        System.out.println("Input the file path:");
+
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+
+        System.out.println(str);
+
         System.out.println(
-                "                    _______ \n" +
-                "                   < hello >\n" +
-                "                    ------- \n" +
-                "            ^__^   /        \n" +
-                "    _______/(oo)  /         \n" +
-                "/\\/(       /(__)            \n" +
-                "   | w----||                \n" +
-                "   ||     ||                ");
+                "            ^__^\n" +
+                "    _______/(oo)\n" +
+                "/\\/(       /(__)\n" +
+                "   | w----||    \n" +
+                "   ||     ||    ");
+
     }
 }
