de9662528404ffe4a759c31a074ab8de26db72f9
Serge Fedosov <107842308+serge-fedosov@users.noreply.github.com>
2022-09-10 13:31:45 +0400

    Stage 3/5 done

--- ASCII Mirror/task/src/asciimirror/Main.java
+++ ASCII Mirror/task/src/asciimirror/Main.java
@@ -1,23 +1,23 @@
 package asciimirror;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         System.out.println("Input the file path:");
 
         Scanner scanner = new Scanner(System.in);
-        String str = scanner.nextLine();
+        String filename = scanner.nextLine();
 
-        System.out.println(str);
-
-        System.out.println(
-                "            ^__^\n" +
-                "    _______/(oo)\n" +
-                "/\\/(       /(__)\n" +
-                "   | w----||    \n" +
-                "   ||     ||    ");
-
+        File file = new File(filename);
+        try (Scanner scannerFile = new Scanner(file)) {
+            while (scannerFile.hasNext()) {
+                System.out.println(scannerFile.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
     }
 }
