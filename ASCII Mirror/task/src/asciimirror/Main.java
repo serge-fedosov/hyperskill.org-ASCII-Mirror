848d1f8d9d883518434782df85e29077d5db5bf7
Serge Fedosov <107842308+serge-fedosov@users.noreply.github.com>
2022-09-10 16:06:41 +0400

    Stage 4/5 done

--- ASCII Mirror/task/src/asciimirror/Main.java
+++ ASCII Mirror/task/src/asciimirror/Main.java
@@ -2,6 +2,8 @@ package asciimirror;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -11,13 +13,33 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         String filename = scanner.nextLine();
 
+        List<String> list = new ArrayList<>();
         File file = new File(filename);
+        int maxLength = 0;
         try (Scanner scannerFile = new Scanner(file)) {
             while (scannerFile.hasNext()) {
-                System.out.println(scannerFile.nextLine());
+                String newStr = scannerFile.nextLine();
+                if (maxLength < newStr.length()) {
+                    maxLength = newStr.length();
+                }
+                list.add(newStr);
             }
         } catch (FileNotFoundException e) {
             System.out.println("File not found");
         }
+
+        for (String str : list) {
+            str = enlargeString(str, maxLength);
+            System.out.println(str + " | " + str);
+        }
+    }
+
+    public static String enlargeString(String str, int length) {
+        StringBuilder sb = new StringBuilder(str);
+        for (int i = sb.length(); i < length; i++) {
+            sb.append(" ");
+        }
+
+        return sb.toString();
     }
 }
